abee5a48276fac49f11c2e82548fb476eb9250a2
Joey <jvanhoek@hotmail.com>
2017-03-11 22:39:53 +0100

    falling works

--- Flying Game/Assets/ControlP1.cs
+++ Flying Game/Assets/ControlP1.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class ControlP1 : MonoBehaviour
 {
+    private Rigidbody RbPlayer;
+    private float yaw;
+    private float pitch;
+    private float velocity;
+    private float addedVelocity;
     
-    Rigidbody RbPlayer;
-    public float PitchClamp = 20;
-    public float RotationSpeed = 5;
-    public float Acceleration = 10;
-    public float speed;
+    public float pitchClamp = 20;
+    public float rotationSpeed = 100;
 
-    private float Yaw;
-    private float Pitch;
-    private float Velocity;
-    
+    public float acceleration = 10;
+    public float desceleration = 20;
+    public float maxVelocity = 3000;
+
     void Start()
     {
         RbPlayer = GameObject.FindGameObjectWithTag("Player1").GetComponent<Rigidbody>();
@@ -23,36 +25,52 @@ public class ControlP1 : MonoBehaviour
     void Update()
     {
         //get yaw and pitch from the controller
-        Yaw += Input.GetAxisRaw("LeftAnalogX");
-        Yaw += Input.GetAxisRaw("KeyLeftRight");
+        yaw += Input.GetAxisRaw("LeftAnalogX");
+        yaw += Input.GetAxisRaw("KeyLeftRight");
 
-        //clamp the camera
-        Pitch = Mathf.Clamp(Pitch, -PitchClamp, PitchClamp);
+        //clamp x rotation
+        //Pitch = Mathf.Clamp(Pitch, -PitchClamp, PitchClamp);
 
         if (Input.GetAxisRaw("KeyUpDown") > 0 || Input.GetAxisRaw("LeftAnalogZ") > 0)
         {
-            Velocity += Input.GetAxisRaw("KeyUpDown") * speed;
-            Velocity += Input.GetAxisRaw("LeftAnalogZ") * speed;
-            
+            velocity += Input.GetAxisRaw("KeyUpDown") * acceleration;
+            velocity += Input.GetAxisRaw("LeftAnalogZ") * acceleration;
+
+            velocity = Mathf.Clamp(velocity, 0, maxVelocity);
+            addedVelocity = velocity;
         }
         else
         {
-            Velocity -= speed * 2;
+            velocity -= desceleration;
+            velocity = Mathf.Clamp(velocity, 0, maxVelocity);
+
+            addedVelocity = 0;
         }
 
-
-        Velocity = Mathf.Clamp(Velocity, 0, 1000);
-
-        print(Velocity +"___"+ Input.GetAxisRaw("KeyUpDown"));
+        print(velocity + "___"+ Input.GetAxisRaw("KeyUpDown"));
 
     }
 
     private void FixedUpdate()
     {
-        Vector3 a = new Vector3(Pitch, Yaw) * RotationSpeed * Time.smoothDeltaTime;
-        RbPlayer.rotation = (Quaternion.Euler(a));
-        Vector3 mov = RbPlayer.transform.forward * Velocity * Time.fixedDeltaTime;
-        RbPlayer.position = RbPlayer.position + mov;
-    }
+        //rotation vector
+        Vector3 a = new Vector3(pitch, yaw) * rotationSpeed;
+        RbPlayer.rotation = (Quaternion.Euler(a * Time.fixedDeltaTime));
+        RbPlayer.AddRelativeForce(Vector3.forward * addedVelocity);
 
+        //store the rigidbody velocity for the gravity
+        Vector3 rigidbodyVelocity = RbPlayer.velocity;
+        RbPlayer.velocity = Vector3.zero;
+
+        
+
+        //make new vector for the velocity in the right direction
+        Vector3 rigidbodyVelocityNoY = new Vector3(rigidbodyVelocity.x, 0, rigidbodyVelocity.z);
+        Vector3 newVelocityVector = transform.forward * rigidbodyVelocityNoY.magnitude;
+        newVelocityVector.y = 0;
+
+        //add gravity to the direction
+        Vector3 actualVelocityVector = new Vector3(0, rigidbodyVelocity.y, 0) + newVelocityVector;
+        RbPlayer.velocity = actualVelocityVector;
+    }
 }
