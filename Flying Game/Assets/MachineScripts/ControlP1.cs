4da8c4904c01796280100f5ad17556e8fbc9d813
Joey <jvanhoek@hotmail.com>
2017-03-16 22:19:58 +0100

    rotaion with the road works, still needs tweaking

--- Flying Game/Assets/MachineScripts/ControlP1.cs
+++ Flying Game/Assets/MachineScripts/ControlP1.cs
@@ -16,6 +16,9 @@ public class ControlP1 : MonoBehaviour
     public float acceleration = 10;
     public float desceleration = 20;
     public float maxVelocity = 3000;
+    public float maxSpeed_Vector = 200;
+
+    RaycastHit hitInfo;
 
     void Start()
     {
@@ -47,26 +50,37 @@ public class ControlP1 : MonoBehaviour
             addedVelocity = 0;
         }
 
-        print(velocity + "___"+ Input.GetAxisRaw("KeyUpDown"));
+        //print(velocity + "___"+ Input.GetAxisRaw("KeyUpDown"));
 
     }
 
     private void FixedUpdate()
     {
-        //rotation vector
-        Vector3 a = new Vector3(pitch, yaw) * rotationSpeed;
-        RbPlayer.rotation = (Quaternion.Euler(a * Time.fixedDeltaTime));
+        Vector3 rayDir = transform.TransformDirection(Vector3.down);
+        Physics.Raycast(transform.position, rayDir, out hitInfo, 5);
+        //raycast position needs a change
+
+        
+
+        Debug.DrawLine(transform.position, transform.position + rayDir * 5, Color.green);
+
+
+        //take normal from surface of the road
+        Quaternion roadNormal = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        //rotation vector multiply with normal of the road and slerp
+        Vector3 rotationInput = new Vector3(pitch, yaw) * rotationSpeed;
+        RbPlayer.rotation =  Quaternion.Slerp(RbPlayer.rotation, roadNormal * Quaternion.Euler(rotationInput * Time.fixedDeltaTime), 0.5f);
         RbPlayer.AddRelativeForce(Vector3.forward * addedVelocity);
 
         //store the rigidbody velocity for the gravity
         Vector3 rigidbodyVelocity = RbPlayer.velocity;
         RbPlayer.velocity = Vector3.zero;
 
-        
-
-        //make new vector for the velocity in the right direction
+        //filter the x and y axes so the falling speed is not added to the forward vector
         Vector3 rigidbodyVelocityNoY = new Vector3(rigidbodyVelocity.x, 0, rigidbodyVelocity.z);
-        Vector3 newVelocityVector = transform.forward * rigidbodyVelocityNoY.magnitude;
+        //make new vector for the velocity in the right direction
+        //clamp is for maximum velocity on de velocity vector
+        Vector3 newVelocityVector = transform.forward * Mathf.Clamp(rigidbodyVelocityNoY.magnitude, 0, maxSpeed_Vector); 
         newVelocityVector.y = 0;
 
         //add gravity to the direction
