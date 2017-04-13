f492114639fada744667fce4e335e6e975ef0d0c
Joey <jvanhoek@hotmail.com>
2017-04-13 19:27:53 +0200

    vectorsss

--- Flying Game/Assets/MachineScripts/ControlP1.cs
+++ Flying Game/Assets/MachineScripts/ControlP1.cs
@@ -20,13 +20,16 @@ public class ControlP1 : MonoBehaviour
     public float maxSpeed_Vector = 200;
     private float addedVelocity;
 
+
     private float fallingSpeed;
+    public float forw;
+    public float fall;
+
     RaycastHit hitInfo;
 
     void Start()
     {
         RbPlayer = GetComponent<Rigidbody>();
-        RbPlayer.useGravity = false;
     }
     
     void Update()
@@ -63,73 +66,55 @@ public class ControlP1 : MonoBehaviour
     private void FixedUpdate()
     {
         Quaternion roadNormal = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-        
+
 
         //take normal from surface of the road
 
         //rotation vector multiply with normal of the road and slerp
         Vector3 rotationInput = new Vector3(pitch, yaw) * rotationSpeed;
         RbPlayer.rotation = Quaternion.Slerp(RbPlayer.rotation, roadNormal * Quaternion.Euler(rotationInput * Time.fixedDeltaTime), 0.5f);
-        //Quaternion.RotateTowards();
-        //Vector3.RotateTowards();
 
-        
-
+        //raycast to detect the road
         Vector3 rayDir = RbPlayer.transform.TransformDirection(Vector3.down);
         bool hit = Physics.Raycast(RbPlayer.transform.position, rayDir, out hitInfo, 2.5f);
         if (hit)
         {
+            //set object above the road
             if (hitInfo.transform.tag.Equals("Road"))
             {
                 RbPlayer.position = hitInfo.point + RbPlayer.transform.up * 2;
             }
         }
         Debug.DrawLine(RbPlayer.transform.position, RbPlayer.transform.position + rayDir * 5, Color.green);
-        Debug.DrawLine(hitInfo.point, hitInfo.point + RbPlayer.transform.up  * 100, Color.blue);
+        Debug.DrawLine(hitInfo.point, hitInfo.point + RbPlayer.transform.up * 100, Color.blue);
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         RbPlayer.AddRelativeForce(Vector3.forward * addedVelocity, ForceMode.Acceleration);
 
         Vector3 vel = RbPlayer.velocity;
-        RbPlayer.velocity = Vector3.zero;
-
-        //Vector3 dir = transform.forward * velocity;
-        //Vector3 fall = Vector3.down * getForce(fallingSpeed);
-
-        
-
-        Vector3 vec = Vector3.Lerp(RbPlayer.velocity, RbPlayer.transform.forward * vel.magnitude, 1);
-
-        Vector3 fell = new Vector3(0, (RbPlayer.transform.TransformDirection(Vector3.down) * fallingSpeed).y, 0);
-        Vector3 vecy = new Vector3(0, vec.y, 0);
-        Vector3 add = fell + vecy;
-
-        vec = new Vector3(vec.x, fell.y, vec.z);
-        RbPlayer.velocity = vec;
+        float y = vel.y;
 
 
+        Vector3 endDir = RbPlayer.transform.forward * vel.magnitude - vel;
         if (hit)
         {
-            fallingSpeed = 0;
+            RbPlayer.useGravity = false;
         }
         else
         {
-            fallingSpeed += 1;
-            //RbPlayer.velocity = Vector3.Lerp(RbPlayer.velocity, RbPlayer.transform.TransformDirection(Vector3.down) * fallingSpeed, Time.fixedDeltaTime);
+            RbPlayer.useGravity = true;
         }
 
+        RbPlayer.velocity += endDir;
         
-        
-        
-        
-        //RbPlayer.transform.TransformDirection(Vector3.down)
+
 
 
         Debug.DrawLine(RbPlayer.transform.position, RbPlayer.transform.position + RbPlayer.velocity * 5, Color.red);
+
         print("velocity: " + RbPlayer.velocity);
-        print("fall"+RbPlayer.transform.TransformDirection(Vector3.down) * fallingSpeed);
-        print("vec: " + vec);
+        print("endDir= " + endDir);
+        print("dir: " + RbPlayer.transform.forward);
     }
 
     public float getForce(float force)
