8759b5cdde36642986dc1be6abd47fcd0f560b67
Joey <jvanhoek@hotmail.com>
2017-04-26 16:51:53 +0200

    finished but not polished

--- Flying Game/Assets/MachineScripts/ControlP1.cs
+++ Flying Game/Assets/MachineScripts/ControlP1.cs
@@ -9,27 +9,39 @@ public class ControlP1 : MonoBehaviour
     private float yaw;
     private float pitch = 0;
     private float velocity;
-    
+
+    public float hoverDistance = 2;
+
     public float pitchClamp = 20;
     public float rotationSpeed = 100;
 
     public float acceleration = 1;
-    public float accelerationDevider = 1;
-    public float desceleration;
+    public float desceleration = 20;
+    public float fallAcceleration = 1;
+
     public float maxVelocity = 3000;
     public float maxSpeed_Vector = 200;
+
     private float addedVelocity;
-
-
+    private float fallingSpeedRayDir;
     private float fallingSpeed;
-    public float forw;
-    public float fall;
+
+    private Vector3 startPos = new Vector3();
+    private Vector3 startRot = new Vector3();
+
+    float z = 0;
 
     RaycastHit hitInfo;
 
     void Start()
     {
         RbPlayer = GetComponent<Rigidbody>();
+        RbPlayer.useGravity = false;
+        RbPlayer.freezeRotation = true;
+        startPos = transform.position;
+        startRot = transform.rotation.eulerAngles;
+
+        //setStart();
     }
     
     void Update()
@@ -37,14 +49,15 @@ public class ControlP1 : MonoBehaviour
         //get yaw and pitch from the controller
         yaw += Input.GetAxisRaw("LeftAnalogX");
         yaw += Input.GetAxisRaw("KeyLeftRight");
+        pitch += Input.GetAxisRaw("LeftAnalogY");
+        pitch += Input.GetAxisRaw("KeyUpDown");
 
         //clamp x rotation
-        //Pitch = Mathf.Clamp(Pitch, -PitchClamp, PitchClamp);
+        pitch = Mathf.Clamp(pitch, -pitchClamp, pitchClamp);
 
-        if (Input.GetAxisRaw("KeyUpDown") > 0 || Input.GetAxisRaw("A") > 0)
+        if (Input.GetAxisRaw("G") > 0 || Input.GetButton("AButton"))
         {
-            velocity += (Input.GetAxisRaw("KeyUpDown") * acceleration) / accelerationDevider;
-            velocity += (Input.GetAxisRaw("A") * acceleration) / accelerationDevider;
+            velocity += acceleration;
 
             velocity = Mathf.Clamp(velocity, 0, maxVelocity);
 
@@ -57,59 +70,62 @@ public class ControlP1 : MonoBehaviour
 
             addedVelocity = 0;
         }
-
-        //print(velocity + "___"+ Input.GetAxisRaw("KeyUpDown"));
-
+        checkDeath();
     }
 
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        Quaternion roadNormal = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-
-
-        //take normal from surface of the road
-
-        //rotation vector multiply with normal of the road and slerp
-        Vector3 rotationInput = new Vector3(pitch, yaw) * rotationSpeed;
-        RbPlayer.rotation = Quaternion.Slerp(RbPlayer.rotation, roadNormal * Quaternion.Euler(rotationInput * Time.fixedDeltaTime), 0.5f);
-
         //raycast to detect the road
         Vector3 rayDir = RbPlayer.transform.TransformDirection(Vector3.down);
-        bool hit = Physics.Raycast(RbPlayer.transform.position, rayDir, out hitInfo, 5f);
+        Vector3 rotationInput = new Vector3();
+        bool hit = Physics.Raycast(RbPlayer.transform.position, rayDir, out hitInfo, 15f);
         if (hit)
         {
             //set object above the road
             if (hitInfo.transform.tag.Equals("Road"))
             {
-                RbPlayer.position = hitInfo.point + RbPlayer.transform.up * 2;
+                if(hitInfo.distance <= hoverDistance+0.5)
+                {
+                    RbPlayer.position = hitInfo.point + RbPlayer.transform.up * hoverDistance;
+                    fallingSpeedRayDir = 0;
+                }
+                else
+                {
+                    fallingSpeedRayDir += 50 / hitInfo.distance + fallingSpeed;
+                }
             }
-        }
-        Debug.DrawLine(RbPlayer.transform.position, RbPlayer.transform.position + rayDir * 5, Color.green);
-        Debug.DrawLine(hitInfo.point, hitInfo.point + RbPlayer.transform.up * 100, Color.blue);
-
-        
-
-        Vector3 vel = RbPlayer.velocity;
-        float y = vel.y;
-        y = 0;
-        
-        if (hit)
-        {
-            RbPlayer.useGravity = false;
-            fallingSpeed = 0;
+            //can't use pitch if on a road
+            rotationInput = new Vector3(0, yaw) * rotationSpeed;
+            pitch = 0;
         }
         else
         {
-            RbPlayer.useGravity = true;
-            fallingSpeed+= 2;
+            //can use pitch in air
+            rotationInput = new Vector3(pitch, yaw) * rotationSpeed;
+            fallingSpeedRayDir = 0;
         }
 
-        RbPlayer.AddRelativeForce(addedVelocity * Vector3.forward);
-        Vector3 endDir = RbPlayer.transform.forward * vel.magnitude - vel;
-        Vector3 fallVec = (fallingSpeed * Vector3.down - endDir).normalized * fallingSpeed;
+        Debug.DrawLine(RbPlayer.transform.position, RbPlayer.transform.position + rayDir * 15, Color.green);
+        //Debug.DrawLine(hitInfo.point, hitInfo.point + RbPlayer.transform.up * 100, Color.blue);
 
-        RbPlayer.AddForce(getAngledForce(endDir + fallVec));
+        //take normal from surface of the road
+        Quaternion roadNormal = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+
+        
+        //rotation vector multiply with normal of the road and slerp
+        RbPlayer.rotation = Quaternion.Lerp(RbPlayer.rotation, (roadNormal * Quaternion.Euler(rotationInput * Time.fixedDeltaTime)), 0.4f);
+
+
+        Vector3 vel = RbPlayer.velocity;
+
+        RbPlayer.AddRelativeForce(addedVelocity * Vector3.forward);
+        float forwardMag = vel.magnitude - fallingSpeed;
+       
+        Vector3 endDir = RbPlayer.transform.forward * vel.magnitude - vel;
+
+        RbPlayer.AddForce(getAngledForce(endDir + Vector3.down * fallingSpeed));
+        RbPlayer.AddForce(getAngledForce(rayDir * fallingSpeedRayDir));
 
         if (RbPlayer.velocity.magnitude > maxSpeed_Vector)
         {
@@ -118,11 +134,37 @@ public class ControlP1 : MonoBehaviour
 
         Debug.DrawLine(RbPlayer.transform.position, RbPlayer.transform.position + RbPlayer.velocity * 5, Color.red);
 
-        print("velocitymag: " + RbPlayer.velocity.magnitude);
+        //print("velocitymag: " + RbPlayer.velocity.magnitude);
+
+
+        if (hit)
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += fallAcceleration;
+        }
     }
 
     public Vector3 getAngledForce(Vector3 force)
     {
         return force * (50 * RbPlayer.mass);
+    }
+
+    public void setStart()
+    {
+        RbPlayer.position = startPos;
+        RbPlayer.rotation = Quaternion.Euler(startRot);
+    }
+
+    public void checkDeath()
+    {
+        if (RbPlayer.position.y < -980)
+        {
+            setStart();
+            RbPlayer.velocity = Vector3.zero;
+        }
+        
     }
 }
--- Flying Game/Assets/MachineScripts/RaceCamera.cs
+++ Flying Game/Assets/MachineScripts/RaceCamera.cs
@@ -12,36 +12,30 @@ public class RaceCamera : MonoBehaviour {
     public float MachineCamDistance = 10;
     public float StdMachineCamHight = 15;
     public float camSpeed = 0.1f;
-
-    Quaternion EnLdocation;
-
-    // Update is called once per frame
-    void Start()
-    {
-
-    }
+    private float refVelY = 0f;
+    private float refVelX = 0f;
+    private float refVelZ = 0f;
 
 
     void LateUpdate () {
 
-        ////get yaw and pitch from the controller
-        //Yaw += Input.GetAxisRaw("RightAnalogX");
-        //Pitch += Input.GetAxisRaw("RightAnalogY");
-
-        ////clamp the camera
-        //Pitch = Mathf.Clamp(Pitch, -PitchClamp, PitchClamp);
-        
         //calculate the rotation of the camera. interpolates to target y rotation
-        Quaternion rot = Quaternion.Slerp(camera.transform.rotation, Quaternion.LookRotation(transform.forward), 1);
+        // Quaternion rot = Quaternion.Slerp(camera.transform.rotation, Quaternion.LookRotation(transform.forward), 0.5f);
         //set y rotation and x rotation
-        
+        float wantedX = transform.rotation.eulerAngles.x;
+        float wantedY = transform.rotation.eulerAngles.y;
+        float wantedZ = transform.rotation.eulerAngles.z;
+
+        wantedY = Mathf.SmoothDampAngle(camera.rotation.eulerAngles.y, wantedY, ref refVelY, 0.08f);
+        wantedX = Mathf.SmoothDampAngle(camera.rotation.eulerAngles.x, wantedX, ref refVelX, 0.08f);
+        wantedZ = Mathf.SmoothDampAngle(camera.rotation.eulerAngles.z, wantedZ, ref refVelZ, 0.08f);
 
         //calculate position of the camera, MUST happen AFTER the rotation or camera will stutter
         Vector3 camPos = transform.position - camera.transform.forward * MachineCamDistance + transform.TransformVector(Vector3.up) * StdMachineCamHight;
-        camera.transform.position = Vector3.Lerp(camera.transform.position, camPos, 1);
 
-        camera.transform.eulerAngles = new Vector3(rot.eulerAngles.x, rot.eulerAngles.y, transform.rotation.eulerAngles.z);
-
+        camera.transform.position = Vector3.Lerp(camera.transform.position, camPos, 0.8f);
+        camera.rotation = Quaternion.Euler(wantedX, wantedY, wantedZ);
+        
 
     }
 }
